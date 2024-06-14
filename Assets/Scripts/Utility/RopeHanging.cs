82066b596e37b9b9ff9b700b0b90215af2022726
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2024-06-14 18:57:55 +0100

    nice

--- Assets/Scripts/Utility/RopeHanging.cs
+++ Assets/Scripts/Utility/RopeHanging.cs
@@ -1,18 +1,45 @@
+using Pelumi.Juicer;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RopeHanging : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement Effect")]
+    [SerializeField] private Vector3 _moveDistance = new Vector3(0, 10f, 0);
+    [SerializeField] private float _movementDuration = 0.5f;
+    [SerializeField] Ease _movementEaseType = Ease.EaseInOutBack;
+
+    private JuicerRuntimeCore<Vector3> _movementEffect;
+
+    private void OnEnable()
     {
-        
+        if (_movementEffect == null)
+        {
+            _movementEffect = transform.JuicyMove(transform.position + _moveDistance, _movementDuration);
+            _movementEffect.SetLoop(0).SetEase(_movementEaseType);
+        }
+
+        _movementEffect.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        if (_movementEffect != null)
+        {
+            _movementEffect.Pause();
+        }
+    }
+
+    public void ToggleActive(bool state)
+    {
+        if (state)
+        {
+            _movementEffect.Start();
+        }
+        else
+        {
+            _movementEffect.Pause();
+        }
     }
 }
