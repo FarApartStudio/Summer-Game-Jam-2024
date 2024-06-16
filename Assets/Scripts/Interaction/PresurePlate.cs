e0efa52563d720758b5114ef703123d24e709191
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2024-06-16 19:10:05 +0100

    Added respawn

--- Assets/Scripts/Interaction/PresurePlate.cs
+++ Assets/Scripts/Interaction/PresurePlate.cs
@@ -1,16 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using Pelumi.Juicer;
 
 public class PresurePlate : MonoBehaviour
 {
+    [Header("Settings")]
+    [SerializeField] private float yPressedPosition;
+    [SerializeField] private float yReleasedPosition;
+    [SerializeField] private Transform plate;
+    [SerializeField] private float moveDuration;
+
+    [Header("Events")]
+    [SerializeField] private UnityEvent OnPressed;
+    [SerializeField] private UnityEvent OnReleased;
+
     private bool isPressed;
+    JuicerRuntimeCore<float> plateEffect;
+
+    private void Awake()
+    {
+        plateEffect = plate.JuicyLocalMoveY(0, moveDuration);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Pilot pilot))
         {
             isPressed = true;
+            OnPressed.Invoke();
+            plateEffect.StartWithNewDestination(yPressedPosition);
         }
     }
 
@@ -19,6 +39,8 @@ public class PresurePlate : MonoBehaviour
         if (other.TryGetComponent(out Pilot pilot))
         {
             isPressed = false;
+            OnReleased.Invoke();
+            plateEffect.StartWithNewDestination(yReleasedPosition);
         }
     }
 }
--- Assets/Scripts/Interaction/ReSpawnTrigger.cs
+++ Assets/Scripts/Interaction/ReSpawnTrigger.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ReSpawnTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform[] spawnPoints;
+
+    private void Awake()
     {
-        
+        spawnPoints = GetComponentsInChildren<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.TryGetComponent(out Pilot pilot))
+        {
+            Transform spawnPoint = GetRandomSpawnPoint();
+            pilot.gameObject.SetActive(false);  
+            pilot.transform.position = spawnPoint.position;
+            pilot.transform.rotation = spawnPoint.rotation;
+            pilot.gameObject.SetActive(true);
+        }
+    }
+
+    private Transform GetRandomSpawnPoint()
+    {
+        return spawnPoints[Random.Range(0, spawnPoints.Length)];
     }
 }
