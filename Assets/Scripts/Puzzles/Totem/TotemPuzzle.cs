01ad21af488fe914b3fca24151601376da812a29
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2024-06-17 23:13:13 +0100

    sxs

--- Assets/Scripts/Puzzles/Totem/TotemPuzzle.cs
+++ Assets/Scripts/Puzzles/Totem/TotemPuzzle.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,13 +13,20 @@ public class TotemPuzzle : MonoBehaviour
 
     private void Awake()
     {
-        totems = GetComponentsInChildren<Totem>();
+        if (totems == null || totems.Length == 0)
+            GenerateTotems();
 
         foreach (var totem in totems)
         {
             totem.SetUp(resetDelay);
             totem.GetHitEvent().AddListener(OnTotemHit);
         }
+    }
+
+    [Button]
+    private void GenerateTotems ()
+    {
+        totems = GetComponentsInChildren<Totem>();
     }
 
     private void OnTotemHit()
