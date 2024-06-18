205e4edc831769e9ef68891cd4317aa3864f1b61
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2024-06-18 19:37:16 +0100

    Double Jump

--- Assets/Scripts/Character/CharacterAnimatorController.cs
+++ Assets/Scripts/Character/CharacterAnimatorController.cs
@@ -15,11 +15,16 @@ public abstract class CharacterAnimatorController : MonoBehaviour
     protected int _animIDJump;
     protected int _animIDFreeFall;
     protected int _animIDMotionSpeed;
-
+    protected int _animIDJumpCount;
 
     public void Start()
     {
         AssignAnimationIDs();
+
+        movementController.OnDoubleJump += () =>
+        {
+            animator.SetTrigger("DoubleJump");
+        };
     }
 
     protected virtual void Update()
@@ -34,6 +39,7 @@ public abstract class CharacterAnimatorController : MonoBehaviour
         _animIDJump = Animator.StringToHash("Jump");
         _animIDFreeFall = Animator.StringToHash("FreeFall");
         _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+        _animIDJumpCount = Animator.StringToHash("JumpCount");
     }
 
     public void HandleAnimation()
@@ -43,6 +49,7 @@ public abstract class CharacterAnimatorController : MonoBehaviour
         animator.SetFloat(_animIDMotionSpeed, movementController.InputMagnitude);
         animator.SetBool(_animIDJump, movementController.IsJumping);
         animator.SetBool(_animIDFreeFall, movementController.FreeFall);
+        animator.SetInteger(_animIDJumpCount, movementController.GetJumpCount);
 
         animator.SetFloat("VelocityX", movementController.InputAnimDirection.x);
         animator.SetFloat("VelocityY", movementController.InputAnimDirection.y);
