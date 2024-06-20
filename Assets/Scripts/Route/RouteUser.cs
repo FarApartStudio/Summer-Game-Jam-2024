9073b450c702a97eeb3c924cb6e3c2144e35a42a
Oluwapelumi Ayeni <57546997+xadegamer@users.noreply.github.com>
2024-06-20 14:51:50 +0100

    Added routes

---
+++ Assets/Scripts/Route/RouteUser.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouteUser : MonoBehaviour
+{
+    public enum MoveType {Loop, Restart}
+
+    [Header("Configuration")]
+    [SerializeField] private bool moveOnRoute = true;
+    [SerializeField] private bool rotateOnRoute = true;
+    [SerializeField] private MoveType moveType;
+
+    [Header("Properties")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private Routes routes;
+    [SerializeField] private Vector3 nextTrainRoute;
+
+    private int currentRouteIndex = 0;
+    public float MoveSpeed => moveSpeed;
+
+    private void Start()
+    {
+        nextTrainRoute = routes.GetRoute(currentRouteIndex);
+    }
+
+    public void Activate(int startIndex, bool canMove = true, bool canRotate = true)
+    {
+        currentRouteIndex = startIndex;
+        nextTrainRoute = routes.GetRoute(currentRouteIndex);
+        moveOnRoute = canMove;
+        rotateOnRoute = canRotate;
+    }
+
+    public void Update()
+    {
+        if (moveOnRoute) MoveToRoute();
+        if (rotateOnRoute) RotateToFaceTarget();
+        if (moveOnRoute || rotateOnRoute) HandleRouteChange();
+    }
+
+    public void MoveToRoute()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextTrainRoute, moveSpeed * Time.deltaTime);
+    }
+
+    public void RotateToFaceTarget()
+    {
+        Quaternion rotation = Quaternion.LookRotation(transform.position - nextTrainRoute);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotateSpeed);
+    }
+
+    public void HandleRouteChange()
+    {
+        if (ReachedNextRoute())
+        {
+            switch (moveType)
+            {
+                case MoveType.Loop:
+                    currentRouteIndex = currentRouteIndex == routes.GetRoutes.Count - 1 ? 0 : ++currentRouteIndex;
+                    break;
+                case MoveType.Restart:
+                    if(currentRouteIndex == routes.GetRoutes.Count - 1)
+                    {
+                        currentRouteIndex = 0;
+                        transform.position = routes.GetRoutes[currentRouteIndex];
+                    }
+                    else ++currentRouteIndex;
+                    break;
+                default:
+                    break;
+            }
+
+            nextTrainRoute = routes.GetRoute(currentRouteIndex);
+        }
+    }
+
+    public bool ReachedNextRoute() => (Vector3.Distance(transform.position, nextTrainRoute) <= 0.2f);
+
+    public void ToggleMovement(bool state) => moveOnRoute = state;
+
+    public void ToggleRotation(bool state) => rotateOnRoute = state;
+}
